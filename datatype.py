a4cfa7268765bdb9ae2c6ef18602e57f909b7740
Vinodhinichokkan <vinodhinichokkan99@gmail.com>
2024-11-21 14:01:29 +0530

    exe

---
+++ datatype.py
@@ -0,0 +1,10 @@
+first = "subscribe"
+print(first[1])
+print(first[-1])
+print(first[1:-1])
+print(first[1:])
+
+# some methods return boolean data
+
+print(first.startswith("s"))
+print(first.endswith("e"))
